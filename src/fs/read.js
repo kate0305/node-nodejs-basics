5fd27d82cb75b04d84039c2bfa59243b8f8755f1
Katsiaryna Voronich <katarinka403@gmail.com>
2022-11-30 21:04:01 +0300

    feat: add solution src/fs read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { __dirname, errMessage } from './utils.js';
+import { join } from 'node:path';
+import { readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+  const pathFile = join(__dirname, 'files', 'new.txt');
+    try {
+      const content = await readFile(pathFile, { encoding: 'utf8' });
+        console.log(content);
+    } catch (error) {
+        throw new Error(errMessage);
+    };
 };
 
 await read();
