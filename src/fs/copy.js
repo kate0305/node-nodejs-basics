6f016e64d05d39788ed4b5cd7402466bbf243884
Katsiaryna Voronich <katarinka403@gmail.com>
2022-12-01 23:24:46 +0300

    feat: add solution src/fs copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { __dirname, errMessage } from './utils.js';
+import { join } from 'node:path';
+import { copyFile, readdir, mkdir } from 'node:fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+  const pathDir = join(__dirname, 'files');
+  const pathCopyDir = join(__dirname, 'files_copy');
+
+  try {
+    await mkdir(pathCopyDir);
+    const files = await readdir(pathDir, {withFileTypes: true});
+    for (const file of files) {
+      const pathFile = join(pathDir, file.name);
+      const pathCopyFile = join(pathCopyDir, file.name);
+      if (file.isFile()) {
+        await copyFile(pathFile, pathCopyFile);
+      } else {
+        await copyDir(pathFile, pathCopyFile);
+      }
+    };
+  } catch (error) {
+    throw new Error(errMessage);
+  };
 };
 
 copy();
