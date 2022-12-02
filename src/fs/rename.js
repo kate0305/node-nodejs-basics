b3ca98af2558e7e8641c9d665a9705281ae7912b
Katsiaryna Voronich <katarinka403@gmail.com>
2022-12-02 13:25:53 +0300

    feat: add solution src/fs rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import { __dirname, errMessage, exist } from './utils.js';
+import { join } from 'node:path';
+import { rename as renameFile } from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+  const pathOldFile = join(__dirname, 'files', 'wrongFilename.txt');
+  const pathNewFile = join(__dirname, 'files', 'properFilename.md');
+  
+  try {
+    if (await exist(pathNewFile)) throw new Error(errMessage);
+    await renameFile(pathOldFile, pathNewFile);
+  } catch (error) {
+    throw new Error(errMessage);
+  }
 };
 
 await rename();
