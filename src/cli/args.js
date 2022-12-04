a6cb0ebf11cb00bef744d8604725903ee7d779ce
Katsiaryna Voronich <katarinka403@gmail.com>
2022-12-04 22:24:16 +0300

    feat: add solution src/cli args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argsCLI = process.argv.slice(2);
+    const args = argsCLI.reduce((acc, arg, index, arr) => {
+      if (i.startsWith('--')) {
+         acc.push(`${arg.slice(2)} is ${arr[index + 1]}`);
+        };
+      return acc;
+    }, []);
+    console.log(args.join(', '));
 };
 
 parseArgs();
