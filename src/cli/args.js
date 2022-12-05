57b65e3c9f700ece500d6ae3d948cc998b6d9e15
Katsiaryna Voronich <katarinka403@gmail.com>
2022-12-06 01:44:53 +0300

    fix: fix mistake in variable name

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,12 +1,12 @@
 const parseArgs = () => {
-    const argsCLI = process.argv.slice(2);
-    const args = argsCLI.reduce((acc, arg, index, arr) => {
-      if (i.startsWith('--')) {
-         acc.push(`${arg.slice(2)} is ${arr[index + 1]}`);
-        };
-      return acc;
-    }, []);
-    console.log(args.join(', '));
+  const argsCLI = process.argv.slice(2);
+  const args = argsCLI.reduce((acc, arg, index, arr) => {
+    if (arg.startsWith('--')) {
+        acc.push(`${arg.slice(2)} is ${arr[index + 1]}`);
+      };
+    return acc;
+  }, []);
+  console.log(args.join(', '));
 };
 
 parseArgs();
