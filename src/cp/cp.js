459bd09095d50998232c6fb2a5a7d49e511c3cb5
Katsiaryna Voronich <katarinka403@gmail.com>
2023-06-19 16:35:36 +0300

    feat: add solution src/cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,10 @@
+import { fork } from 'node:child_process';
+import { join } from 'node:path';
+import { getPath } from '../utils.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathFile = join(getPath(import.meta.url), 'files', 'script.js');
+  fork(pathFile, args);
 };
 
-spawnChildProcess();
+spawnChildProcess(['1', '2', '3']);
